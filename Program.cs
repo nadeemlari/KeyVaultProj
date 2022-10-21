503c03af69810702aaa2653f79344382ae2ab6fd
Mohammad Nadeem <nadeemlari@hotmail.com>
2022-10-21 20:55:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+var credential = new DefaultAzureCredential( new DefaultAzureCredentialOptions
+{
+     ManagedIdentityClientId = "c1dd26a4-63fb-4231-b082-d54b271be2c9"
+});
+var client = new SecretClient(new Uri("https://mnlkv.vault.azure.net/"), credential);
+var secret = await client.GetSecretAsync("Test");
+string actualSecret = secret.Value.Value;
+Console.WriteLine( "done");
